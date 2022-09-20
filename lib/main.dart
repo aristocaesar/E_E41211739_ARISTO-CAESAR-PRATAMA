7a3f8607f254025500aa2d69857b8222966e9720
aristocaesar <hi@aristoc.space>
2022-09-20 13:37:10 +0700

    finish game werewolf

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'dart:convert';
+
 // Ternary
 void cekNilai(int number) {
   const kkm = 75;
@@ -10,11 +13,62 @@ void cekNilai(int number) {
   }
 }
 
+// Werewolf
+void wereWolf() {
+  // Value
+  try {
+    bool main = true;
+    do {
+      print("Masukkan nama : ");
+      var nama = stdin.readLineSync(encoding: utf8);
+      if (nama.toString() == "") {
+        throw "Nama harus terisi!";
+      }
+      print("Pilihan Peran");
+      print("1.Penyihir");
+      print("2.Guard");
+      print("3.Werewolf");
+      print("Masukkan peran ( no.urutan ) : ");
+      var peran = stdin.readLineSync(encoding: utf8);
+      switch (peran.toString()) {
+        case "1":
+          print(
+              'Selamat datang di Dunia Werewolf, $nama - Halo penyihir $nama , kamu dapat melihat siapa yang menjadi werewolf!.');
+          break;
+        case "2":
+          print(
+              'Selamat datang di Dunia Werewolf, $nama - Halo guard $nama , kamu akan membantu melindungi temanmu dari serangan werewolf.');
+          break;
+        case "3":
+          print(
+              'Selamat datang di Dunia Werewolf, $nama - Halo werewolf $nama , kamu akan membantu melindungi temanmu dari serangan werewolf.');
+          break;
+        default:
+          throw "$nama , Peran yang kamu pilih tidak tersedia!";
+      }
+      if (peran.toString() == "") {
+        throw "Peran harus dipilih!";
+      }
+      print("Apakah anda ingin bermain lagi ? (y/n) ");
+      var lagi = stdin.readLineSync(encoding: utf8);
+      if (lagi != "y") {
+        main = false;
+        print("Bye!");
+      }
+    } while (main);
+  } catch (e) {
+    print(e);
+    print("Game gagal dimaikan !");
+  }
+}
+
 // Perulangan Maju Mundur
 void majuMundur(int number) {
+  // Increment
   for (int i = 1; i <= number; i++) {
     print(i);
   }
+  // Decrement
   for (int i = number; i >= 1; i--) {
     print(i);
   }
@@ -23,6 +77,9 @@ void majuMundur(int number) {
 void main(List<String> args) {
   print("---1.TERNARY---");
   cekNilai(75);
+  print("-------------");
+  print("---2.WEREWOLF---");
+  wereWolf();
   print("-------------");
   print("---3.PERULANGAN MAJU MUNDUR---");
   majuMundur(5);
